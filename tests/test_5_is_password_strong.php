ac3a731635f2cab2097a0db1be39d2da4400a375
KrishBrown <h.soundararadjou@gmail.com>
2023-04-06 14:38:51 +0200

    Add files via upload

--- tests/test_5_is_password_strong.php
+++ tests/test_5_is_password_strong.php
@@ -4,7 +4,6 @@
     $r1=create_user("usernameki","password");
     $r2=create_user("usernamekj","p@ssword");
     $r3=create_user("usernamekk","p@ssword1");
-    printf("1=$r1\n2=$r2\n3=$r3\n");
     if($r1==5 && $r2==5 && $r3==0)
         printf("true\n");
     else
