f76f78b0f2a1c85bcd1297a7b932d4c68f5abc08
KrishBrown <h.soundararadjou@gmail.com>
2023-04-06 13:58:18 +0200

    Add files via upload

--- tests/test_1_is_empty.php
+++ tests/test_1_is_empty.php
@@ -5,7 +5,7 @@
     $r2=create_user("","");
     $r3=create_user("usernamek","");
     $r4=create_user("usernameka","p@ssword1");
-    #printf("1=$r1\n2=$r2\n3=$r3\n4=$r4\n");
+    printf("1=$r1\n2=$r2\n3=$r3\n4=$r4\n");
     if($r1==1 && $r2==1 && $r3==1 && $r4 == 0)
         printf("true\n");
     else
--- tests/test_3_is_too_long.php
+++ tests/test_3_is_too_long.php
@@ -5,7 +5,7 @@
     $r2=create_user("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaab","bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
     $r3=create_user("usernamekd","bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
     $r4=create_user("usernameke","p@ssword1");
-    #printf("1=$r1\n2=$r2\n3=$r3\n4=$r4\n");
+    # printf("1=$r1\n2=$r2\n3=$r3\n4=$r4\n");
     if($r1==3 && $r2==3 && $r3==3 && $r4 == 0)
         printf("true\n");
     else
