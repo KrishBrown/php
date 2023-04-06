fd040158b81740d4b9fcee1eae5f4c908966e685
KrishBrown <h.soundararadjou@gmail.com>
2023-04-06 12:58:44 +0200

    Update test_3_is_too_long.php

--- tests/test_3_is_too_long.php
+++ tests/test_3_is_too_long.php
@@ -9,7 +9,7 @@
     $r4=create_user("username".$rtemp,"p@ssword1");
 
     if($r1==3 && $r2==3 && $r3==3 && $r4 == 0)
-        printf("true\n");
+        printf("1=$r1\n2=$r2\n3=$r3\n4=$r4\ntrue\n");
     else
         printf("false\n");
 ?>
